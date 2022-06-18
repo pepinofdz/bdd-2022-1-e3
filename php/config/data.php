ac49e62ee802aed8cfe69622b8ec8ee17895c985
ElTioAndresCabezas <ascabezas@uc.cl>
2022-06-18 12:45:24 -0400

    Creacion de PHP

---
+++ php/config/data.php
@@ -0,0 +1,11 @@
+<?php
+    // Variables para hacer iniciar sesión
+    $user1 = 'grupo67';
+    $password1 = 'DCCalmao';
+    $db_name1 = 'grupo67e3';
+
+    // Variables para hacer iniciar sesion con 2da base de datos
+    $user2 = 'grupo48';
+    $password2 = 'tatakae!';
+    $db_name2 = 'grupo48e3';
+?>
---
+++ php/index.php
@@ -0,0 +1,16 @@
+<?php
+    include("templates/header.html");
+?>
+
+<head>
+    <link rel="stylesheet" href="./styles/style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">
+</head>
+
+<body>
+    <p>Hola Mundo!</p>
+
+
+</body>
+
+</html>
