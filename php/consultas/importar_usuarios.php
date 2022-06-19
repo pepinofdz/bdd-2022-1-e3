bf74bb19ed9aed911cfc4506845f4d93419f3717
SebaPob <sebapob@uc.cl>
2022-06-19 19:04:21 -0400

    Creación de páginas para iniciar sesión y redirect a index

---
+++ php/consultas/importar_usuarios.php
@@ -0,0 +1,21 @@
+
+
+
+
+<?php
+    require("../config/conection.php");
+    // $query = "INSERT INTO
+    $query = "SELECT *
+                FROM poke1;"; // Crear la consulta
+    $result = $db -> prepare($query);
+    $result -> execute();
+
+    $data = $result -> fetchAll();
+    
+?>
+
+
+
+    <?php
+    header('Refresh: 0; url = ../index.php')
+    ?>
--- php/index.php
+++ php/index.php
@@ -1,3 +1,10 @@
+<?php session_start();
+    if (isset($_SESSION['username'])){
+        echo "Bienvenido/a: ";
+        echo $_SESSION['username'];
+    }
+?>
+
 <?php
     include("templates/header.html");
 ?>
@@ -8,8 +15,39 @@
 </head>
 
 <body>
-    <p>Hola Mundo!</p>
+    <h1> Entrega 3</h1>
+    <br>
+    <?php
+        if (!isset($_SESSION['username'])) {
+    ?>
+        <form align="center" action="views/login.php" method="get">
+            <input type="submit" value="Iniciar sesión">
+        </form>
 
+        <form align="center" action="consultas/importar_usuarios1.php" method="post">
+            <input type="submit" value="Importar Usuarios">
+        </form>
+
+    <?php } else { ?>
+        <form align="center" action="views/logout.php" method="post">
+            <input type="submit" value="Cerrar sesión">
+        </form>
+
+        <form align="center" action="consultas/pokemones.php" method="post">
+            <input type="submit" value="Ver pokemones">
+        </form>
+
+        <form align="center" action="consultas/pelea_pokemon.php" method="post">
+            <input type="submit" value="Ver peleas">
+        </form>
+
+        <form align="center" action="consultas/crear_pelea_pokemon.php" method="post">
+            <input type="text" name="pid1">
+            <input type="text" name="pid2">
+            <input type="submit" value="Crear pelea">
+        </form>
+
+    <?php } ?>
 
 </body>
 
