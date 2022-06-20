3e4cb71f920edb622f18f84224fb918f8d129d4f
ElTioAndresCabezas <ascabezas@uc.cl>
2022-06-19 20:50:19 -0400

    Creacion de usuarios de aerolineas

--- php/consultas/importar_usuarios.php
+++ php/consultas/importar_usuarios.php
@@ -19,7 +19,25 @@
         $data = $result -> fetchAll();
     }
 
-    header('Refresh: 0; url = ../index.php')
+    //Creando usuarios de companias
+    $query = "SELECT * FROM compania;";
+    $result = $db1 -> prepare($query);
+    $result -> execute();
+    
+    $data = $result -> fetchAll();
+
+    foreach ($data as $a) {
+        $rand_pass = '';
+        for ($i = 0; $i < 9; $i++) {
+            $rand_pass .= strval(rand(0,9));
+        }
+        $user = strval($a[0]);
+        $query = "INSERT INTO usuario(username, password, tipo) VALUES ( '$user' , '$rand_pass' , 2 );";
+        $result = $db1 -> prepare($query);
+        $result -> execute();
+    }
+
+    //header('Refresh: 0; url = ../index.php')
 ?>
 
 
